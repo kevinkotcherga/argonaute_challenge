e7c96244e529223b44730320f0af08cc54a9fbd0
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-06 15:06:35 +0200

    feat(slice) : ajout du slice argonaute

---
+++ client/src/features/argonaute/argonautesSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const argonautesSlice = createSlice({
+	// 1) UN NOM
+	name: 'argonautes',
+	// 2) STATE DE BASE
+	initialState: {
+    // Le state de base est nul, il sera ensuite incrémenté avec la base de données
+    argonautes: null,
+  },
+	// 3) REDUCERS
+	reducers: {
+		// GET
+		setArgonautesData: (state, action) => {
+			// Quand l'action est appelée, le state est récupéré (la data, au début de base nul)
+			// action.payload sont les données que l'on récupère en paramètre
+			state.argonautes = action.payload;
+		},
+		// CREATE
+		addArgonaute: (state, action) => {
+			// push ajoute l'action que l'on récupère en paramètre au state
+			state.argonautes.push(action.payload);
+		},
+	},
+});
+
+// 4) EXPORT
+// Les reducers sont exportés pour être utilisé dans les composants
+export const { setArgonautesData, addArgonaute } =
+	argonautesSlice.actions;
+// pisctureSlice
+export default argonautesSlice.reducer;
