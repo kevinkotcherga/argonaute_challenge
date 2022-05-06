16e8ba6920e1faea76e5e73c6bd2d0cbe57ac765
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-06 11:07:05 +0200

    feat(index) : connexion à mongodb

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+// Importation de la DB mongodb avec mongoose
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('DBconnextion Successfull!'))
+	.catch(err => console.log(err));
+
 // Connexion au serveur
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!")
