7c66fc78b1d29321637e74eb69dc4b59f8cef8d6
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-06 11:05:24 +0200

    feat(index) : création d'index.js et importation des configurations

---
+++ index.js
@@ -0,0 +1,8 @@
+// Importation des configurations
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+// dotenv permet de récupérer les informations inscritent dans .env
+const dotenv = require('dotenv');
+dotenv.config();
