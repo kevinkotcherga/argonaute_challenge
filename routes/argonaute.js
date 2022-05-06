7b9a343bc0cfe5add07d73179cacd637fff32e62
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-06 11:12:37 +0200

    feat(routes) : création de la route argonaute

---
+++ routes/argonaute.js
@@ -0,0 +1,10 @@
+// Importation des configurations
+const express = require('express');
+const router = express.Router();
+
+// Importation du model Argonaute
+const Argonaute = require('../models/Argonaute');
+
+
+// Exportation des routes
+module.exports = router;
