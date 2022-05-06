79c1db82d89dd1330644fa3edc2434f5317d879e
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-06 11:11:29 +0200

    feat(models) : création du model Argonaute

---
+++ models/Argonaute.js
@@ -0,0 +1,13 @@
+// Importation des configurations
+const mongoose = require('mongoose');
+
+// Le model d'Argonaute contiendra obligatoirement un nom de type "string" pour être valide
+const ArgonauteSchema = new mongoose.Schema(
+	{
+		name: { type: String, required: true, unique: true },
+	},
+	{ timestamps: true },
+);
+
+// Exportation du model
+module.exports = mongoose.model('Argonaute', ArgonauteSchema);
