1b3596744019a535c22421e5167816eb4ac300cf
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-06 11:06:10 +0200

    feat(index) : connexion au serveur avec app.listen

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const cors = require('cors');
 // dotenv permet de récupérer les informations inscritent dans .env
 const dotenv = require('dotenv');
 dotenv.config();
+
+// Connexion au serveur
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Backend server is running!")
+})
