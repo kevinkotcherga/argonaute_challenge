8db2a154dd5a9846a1ab8451534506641d5d4835
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-06 12:09:42 +0200

    feat(components) : ajout du component Header

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,8 +1,11 @@
+import Header from '../../components/header/Header';
 import './home.scss';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <Header />
+    </div>
   )
 }
 
