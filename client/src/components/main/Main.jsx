1b4243c0e875a04502468af928eac2dc988bac26
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-05-06 15:04:08 +0200

    feat(components) : ajout du component Main

---
+++ client/src/components/main/Main.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import { useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addArgonaute } from '../../feature/argonautesSlice';
+import MemberList from '../memberList/MemberList';
+import './main.scss';
+import { addArgonaute } from '../../store';
+
+const Main = () => {
+  const [name, setName] = useState("");
+  const dispatch = useDispatch();
+  const formRef = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      name
+    };
+    console.log(data);
+    axios.post('http://localhost:5000/argonautes', data).then((res) => {
+			dispatch(addArgonaute(res.data));
+		});
+    formRef.current.reset();
+  };
+
+  return (
+    <div className='main'>
+      <h2>Ajouter un(e) Argonaute</h2>
+        <form className="main__form" onSubmit={handleSubmit} ref={formRef}>
+          <label>Nom de l'Argonaute</label>
+          <input id="name" name="name" type="text" placeholder="Charalampos" onChange={(e) => setName(e.target.value)}/>
+          <button type="submit">Envoyer</button>
+        </form>
+        <MemberList />
+    </div>
+  );
+};
+
+export default Main;
